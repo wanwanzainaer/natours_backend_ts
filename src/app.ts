e199c42a186b15d4ab3d7290696495079fbdbac3
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-12-13 19:33:59 -0800

    add tours interface

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,34 @@
 import express, { Request, Response } from 'express';
-
+import fs from 'fs';
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res
-    .status(200)
-    .json({ message: 'Hello world from the server side!!!', app: 'Natours' });
-});
+interface tour {
+  id: number;
+  name: string;
+  duration: number;
+  maxGroupSize: 25;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: Date[];
+}
 
-app.post('/', (req: Request, res: Response) => {
-  res.send('You can post to this end point');
+const tours: tour[] = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`).toString()
+);
+
+app.get('/api/v1/tours', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 5000;
