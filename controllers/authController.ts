a127b6e6c0eca412b83f017b8a3330341abf29b6
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-12-23 22:25:53 -0800

    Add signup with the auth jwt token

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,12 +1,28 @@
+import jwt from 'jsonwebtoken';
 import { NextFunction, Response, Request } from 'express';
 import { User } from '../models/user';
 import { catchAsync } from '../utils/catchAsync';
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = await User.build(req.body);
+    const { name, email, password, passwordConfirm } = req.body;
+    const newUser = await User.build({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+
+    const token = jwt.sign(
+      { id: newUser.id },
+      process.env.JSON_WEBTOKEN_KEY as jwt.Secret,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
--- models/user.ts
+++ models/user.ts
@@ -24,48 +24,59 @@ interface UserModel extends mongoose.Model<UserDoc> {
 
 const Schema = mongoose.Schema;
 
-const userShcema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'User must has a name, which can not be empty'],
-  },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    required: [
-      true,
-      'User must has a email which will be account, which can not be empty',
-    ],
-    validate: [
-      validator.isEmail,
-      'This email is not valid email, Pleas provide a valid email',
-    ],
-  },
-  photo: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: [true, 'A User must have password to save user account safty'],
-    minlength: 8,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please Confirm your password'],
-    validate: {
-      // this only work on CREATE and SAVE!!!
-      validator: function (this: UserDoc, el: string): boolean {
-        return this.password === el;
+const userShcema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User must has a name, which can not be empty'],
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [
+        true,
+        'User must has a email which will be account, which can not be empty',
+      ],
+      validate: [
+        validator.isEmail,
+        'This email is not valid email, Pleas provide a valid email',
+      ],
+    },
+    photo: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, 'A User must have password to save user account safty'],
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please Confirm your password'],
+      validate: {
+        // this only work on CREATE and SAVE!!!
+        validator: function (this: UserDoc, el: string): boolean {
+          return this.password === el;
+        },
+        message: 'Password are not the same!',
       },
-      message: 'Password are not the same!',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
     },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 userShcema.pre<UserDoc>('save', async function (next) {
   // Only run this function if password was actually modified
