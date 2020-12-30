30f07e1e6c9e3aaa2cd66e15d0571fe53d7d2e66
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-12-29 22:33:24 -0800

    Add the the

--- models/tour.ts
+++ models/tour.ts
@@ -125,6 +125,30 @@ const tourSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: {
