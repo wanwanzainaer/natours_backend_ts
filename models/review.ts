3495d5c8ee8aa5786a09cad92698407986a98002
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-12-31 00:56:20 -0800

    Add the review model and typescript

---
+++ models/review.ts
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export interface ReviewAttrs {
+  review: string;
+  rating: number;
+  tour: string;
+  user: string;
+}
+export interface ReviewDoc extends mongoose.Document {
+  review: string;
+  rating: number;
+  // createdAt: Date;
+  tour: string;
+  user: string;
+}
+
+interface ReviewModel extends mongoose.Model<ReviewDoc> {
+  build(attrs: ReviewAttrs): ReviewDoc;
+}
+
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'A review must have rating'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A review must belong a tour'],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must belong a User'],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    toObject: { virtuals: true },
+  }
+);
+
+reviewSchema.statics.build = (attrs: ReviewAttrs) => {
+  return Review.create(attrs);
+};
+
+export const Review = mongoose.model<ReviewDoc, ReviewModel>(
+  'Review',
+  reviewSchema
+);
