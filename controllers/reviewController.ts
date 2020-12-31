3e05608074a22e80d478fbd46f91334c68dd28bc
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-12-31 14:18:17 -0800

    add the get tourId reviews

--- controllers/reviewController.ts
+++ controllers/reviewController.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Response, Request } from 'express';
 import { Review } from '../models/review';
 import { catchAsync } from '../utils/catchAsync';
-
+import { AuthRequestUser } from '../controllers/authController';
 export const getAllReviews = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const reviews = await Review.find();
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const reviews = await Review.find(filter);
 
     res.status(200).json({
       status: 'success',
@@ -16,7 +18,11 @@ export const getAllReviews = catchAsync(
   }
 );
 export const createReview = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: AuthRequestUser, res: Response, next: NextFunction) => {
+    // Allow nested routes
+    if (!req.body.tour) req.body.tour = req.params.tourId;
+    if (!req.body.user) req.body.user = req.user!.id;
+
     const newReview = await Review.build(req.body);
 
     res.status(201).json({
--- routes/tourRoutes.ts
+++ routes/tourRoutes.ts
@@ -10,8 +10,12 @@ import {
   getMonthlyPlan,
 } from '../controllers/tourController';
 import { protect, restrictTo } from '../controllers/authController';
+import { reviewRouter } from './reviewRoutes';
 
 const tourRouter = express.Router();
+
+tourRouter.use('/:tourId/reviews', reviewRouter);
+
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 tourRouter.route('/tour-stats').get(getTourStats);
 tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
